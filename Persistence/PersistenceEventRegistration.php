d63cccf47bbe60a627c4ea3841eac508818414f7
rockandsalt <marc.wang@mail.mcgill.ca>
2016-01-26 12:26:20 -0500

    W05 persistence

--- Persistence/PersistenceEventRegistration.php
+++ Persistence/PersistenceEventRegistration.php
@@ -1,1 +1,22 @@
 <?php
+class PersistenceEventRegistration {
+	private $filename;
+	function __construct($filename = 'data.txt') {
+		$this->filename = $filename;
+	}
+	function loadDataFromStore() {
+		if (file_exists($this->filename)) {
+			$str = file_get_contents($this->filename);
+			$rm = unserialize($str);
+		} else {
+			$rm = RegistrationManager::getInstance();
+		}
+		return $rm;
+	}
+	function writeDataToStore($rm) {
+		
+		$str = serialize($rm);
+		file_put_contents($this->filename, $str);
+	}
+}
+?>
---
+++ test/PersistenceEventRegistrationTest.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'C:\Users\marc\Documents\EclipseWorkspace\EventRegistrationWeb\Persistence\PersistenceEventRegistration.php';
+require_once 'C:\Users\marc\Documents\EclipseWorkspace\EventRegistrationWeb\Model\Participant.php';
+require_once 'C:\Users\marc\Documents\EclipseWorkspace\EventRegistrationWeb\Model\RegistrationManager.php';
+
+
+class PersistenceEventRegistrationTest  extends PHPUnit_Framework_TestCase
+{
+	protected $pm;
+	
+	protected function setUp()
+	{
+		$this -> pm = new PersistenceEventRegistration();
+		
+	}
+	
+	protected function tearDown()
+	{
+		
+	}
+	
+	public function testPersistence()
+	{
+		$rm = RegistrationManager :: getInstance();
+		$participant = new Participant("Frank");
+		$rm -> addParticipant($participant);
+		
+		$this -> pm -> writeDataToStore($rm);
+		
+		$rm -> delete();
+		
+		$this -> assertEquals(0, count($rm->getParticipants()));
+		
+		$rm = $this -> pm-> loadDataFromStore();
+		$this -> assertEquals(1, count($rm->getParticipants()));
+		$myParticipant = $rm -> getParticipant_index(0);
+		$this -> assertEquals("Frank", $myParticipant->getName());
+		
+	}
+	
+	
+}
